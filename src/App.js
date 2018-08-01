90f3eadeb43641e1abe274ed1cf7b68d41a4b063
Kevin Hubert <iamkevinhubert@gmail.come>
2018-08-01 16:52:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Contact from './components/Contact';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header branding="Contact Manager" />
+        <Contact
+          name="Kevin Hubert"
+          email="getoffmylawn@gmail.com"
+          phone="555.555.5555"
+        />
+        <Contact
+          name="Watson Dog"
+          email="woof@gmail.com"
+          phone="111.111.1111"
+        />
+        <Contact />
       </div>
     );
   }
